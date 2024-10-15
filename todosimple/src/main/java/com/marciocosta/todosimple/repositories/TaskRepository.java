5b658d39027bb7133db9e92f1124dde856440567
Marcio Costa <marcio-costa38@hotmail.com>
2024-10-14 21:31:53 -0300

    Adicionando a classe TaskRepository

---
+++ todosimple/src/main/java/com/marciocosta/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,24 @@
+package com.marciocosta.todosimple.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.repository.query.Param;
+
+import com.marciocosta.todosimple.models.Task;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    
+    //1° forma de escrever(tudo automatizado pelo Spring)
+    List<Task> findByUser_Id(Long id);
+
+    //2° forma de escrever(Jeito Semi-automatico de fazer usando comando JPQL)
+    // @Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")
+    // List<Task> findByUser_Id(@Param("id") Long id);
+
+    //3° forma de escrever(Usando SQL nativo)
+    // @Query(value = "SELECT * FROM task t WHERE t.user_id = :id", nativeQuery = true)
+    // List<Task> findByUser_Id(@Param("id") Long id);
+
+}
---
+++ todosimple/src/main/java/com/marciocosta/todosimple/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.marciocosta.todosimple.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marciocosta.todosimple.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
